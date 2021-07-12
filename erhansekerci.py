fae2c309c7fea68786e72aee67e5076a10579ce6
sekerhan <85903337+sekerhan@users.noreply.github.com>
2021-07-12 21:13:00 +0300

    Update erhansekerci.py
    düzenleme yapıyorum

--- erhansekerci.py
+++ erhansekerci.py
@@ -1,4 +1,10 @@
-kimlik = "Erhan Sekerci"
+“kimlik” Sınıfı: 
+    def_init_(self,ad,Soyad);
+      
+         self.Ad=“Erhan”
+         self.Soyad=“Şekerci”
+
+kimlik = ad + soyad
 
 
 for i in range (10):
